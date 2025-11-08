62480fdf63a740bc4ecbb87e0bd1ba6457706de3
aymanshawqi360 <aymanshawqi380gmail.com>
2025-11-08 09:12:08 +0300

    -update MainActivity to support motivational shake feature

--- android/app/src/main/kotlin/com/example/shake_detection_app/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/shake_detection_app/MainActivity.kt
@@ -1,5 +1,15 @@
+
+
 package com.example.shake_detection_app
 
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+import com.example.shake_detection_app.ShakeDetectorPlugin
 
-class MainActivity : FlutterActivity()
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine)
+        flutterEngine.plugins.add(ShakeDetectorPlugin())
+    }
+}
